5960593f33d33ede2499f5bab9af97773fe78e14
Jon Klixbüll Langeland <jon@klixbull.org>
2016-01-02 20:08:58 +0100

    Multiple updares

--- Libraries/Langeland/Bazinga/Command/CreateCommand.php
+++ Libraries/Langeland/Bazinga/Command/CreateCommand.php
@@ -4,6 +4,15 @@ namespace Langeland\Bazinga\Command;
 
 class CreateCommand extends \Langeland\Bazinga\Command\AbstractCommand {
 
+	protected $configuration = array(
+		'sitesAvailable.directory' => '/Users/langeland/temp/hoostroot/sites-available',
+		'sitesEnabled.directory' => '/Users/langeland/temp/hoostroot/sites-enabled',
+		'fpmPool.directory' => '/Users/langeland/temp/hoostroot/pool.d',
+		'hostroot.directory' => '/Users/langeland/temp/hoostroot/sites'
+	);
+
+	protected $virtualHostConfiguration = array();
+
 	public function __construct() {
 		parent::__construct();
 	}
@@ -20,21 +29,19 @@ class CreateCommand extends \Langeland\Bazinga\Command\AbstractCommand {
 
 		$helper = $this->getHelper('question');
 
-		$configuration = array();
 		/***************************************************************************************************************
 		 *
 		 **************************************************************************************************************/
-		$question = new \Symfony\Component\Console\Question\Question('Input main domain name. (no http://): ');
-		$question->setValidator(function ($answer) {
-			if ($answer == '') {
-				throw new \RuntimeException('Domain name cannot be empty');
-			}
-			return $answer;
-		});
-
-		$configuration['domain'] = trim($helper->ask($input, $output, $question));
-		$configuration['directory'] = implode('.',array_reverse(explode('.',$configuration['domain'])));
-
+//		$question = new \Symfony\Component\Console\Question\Question('Input main domain name. (no http://): ');
+//		$question->setValidator(function ($answer) {
+//			if ($answer == '') {
+//				throw new \RuntimeException('Domain name cannot be empty');
+//			}
+//			return $answer;
+//		});
+//
+//		$this->virtualHostConfiguration['domain'] = trim($helper->ask($input, $output, $question));
+//		$this->virtualHostConfiguration['directory'] = implode('.', array_reverse(explode('.', $this->virtualHostConfiguration['domain'])));
 
 		/***************************************************************************************************************
 		 *
@@ -44,23 +51,20 @@ class CreateCommand extends \Langeland\Bazinga\Command\AbstractCommand {
 			if ($answer == '') {
 				throw new \RuntimeException('Installation name cannot be empty');
 			}
-			if(in_array($answer . '_web', $this->getSystemUsers())){
+			if (in_array($answer . '_web', $this->getSystemUsers())) {
 				throw new \RuntimeException('User exists');
 			}
 
-			if(in_array($answer . '_grp', $this->getSystemGroups())){
+			if (in_array($answer . '_grp', $this->getSystemGroups())) {
 				throw new \RuntimeException('Group exists');
 			}
 
 			return $answer;
 		});
 
-		$configuration['name'] = trim($helper->ask($input, $output, $question));
-
-		$configuration['user'] = $configuration['name'] . '_web';
-		$configuration['group'] = $configuration['name'] . '_grp';
-
-
+		$this->virtualHostConfiguration['name'] = trim($helper->ask($input, $output, $question));
+		$this->virtualHostConfiguration['user'] = $this->virtualHostConfiguration['name'] . '_web';
+		$this->virtualHostConfiguration['group'] = $this->virtualHostConfiguration['name'] . '_grp';
 
 		/***************************************************************************************************************
 		 *
@@ -71,38 +75,148 @@ class CreateCommand extends \Langeland\Bazinga\Command\AbstractCommand {
 			$pseudoGroups
 		);
 		$question->setErrorMessage('Pseudo group %s is invalid.');
-		$configuration['pseudoGroups'] = $helper->ask($input, $output, $question);
-
+		$this->virtualHostConfiguration['pseudoGroup'] = $helper->ask($input, $output, $question);
 
 		/***************************************************************************************************************
 		 *
 		 **************************************************************************************************************/
-		print_r($configuration);
+		print_r($this->virtualHostConfiguration);
 
+		print_r(array(
+			'rootDirectory' => '/home/hostroot/' . $this->virtualHostConfiguration['pseudoGroup'] . '/' . $this->virtualHostConfiguration['name'],
+			'create user' => $this->virtualHostConfiguration['user'],
+			'create group' => $this->virtualHostConfiguration['group']
+		));
+
+		$this->create();
 	}
 
-
-	private function getSystemUsers(){
+	private function getSystemUsers() {
 		$process = new \Symfony\Component\Process\Process('cat /etc/passwd | cut -d: -f1');
 		$process->run();
-		foreach(explode("\n", $process->getOutput()) AS $line) {
-			if($line != '' && substr($line, 0, 1) != '#'){
+		foreach (explode("\n", $process->getOutput()) AS $line) {
+			if ($line != '' && substr($line, 0, 1) != '#') {
 				$users[] = $line;
 			}
 		}
 		return $users;
 	}
 
-	private function getSystemGroups(){
+	private function getSystemGroups() {
 		$process = new \Symfony\Component\Process\Process('cat /etc/group | cut -d: -f1');
 		$process->run();
-		foreach(explode("\n", $process->getOutput()) AS $line) {
-			if($line != '' && substr($line, 0, 1) != '#'){
+		foreach (explode("\n", $process->getOutput()) AS $line) {
+			if ($line != '' && substr($line, 0, 1) != '#') {
 				$groups[] = $line;
 			}
 		}
 		return $groups;
 	}
+
+	private function create() {
+		$this->taskCreateDirectories();
+		$this->taskCreateFpmFile();
+		$this->taskCreateVirtualHostFile();
+
+		$this->taskCreateSystemUserAndGroup();
+		$this->taskEnableSite();
+		$this->taskRestartApache();
+	}
+
+	private function taskCreateDirectories() {
+		$this->output->writeln('Creating Directories');
+
+		$pseudoGroupDirectory = $this->configuration['hostroot.directory'] . '/' . $this->virtualHostConfiguration['pseudoGroup'];
+		$virtualHostDirectory = $pseudoGroupDirectory . '/' . $this->virtualHostConfiguration['name'];
+
+		if (!is_dir($this->configuration['hostroot.directory'] . '/' . $this->virtualHostConfiguration['pseudoGroup'])) {
+			die('Missing pseudoGroup dir');
+		}
+
+		if (!mkdir($virtualHostDirectory, 0777)) {
+			throw new \Exception('Failed to create folder: ' . $virtualHostDirectory);
+		}
+		if (!mkdir($virtualHostDirectory . '/system/', 0777, TRUE)) {
+			throw new \Exception('Failed to create folder: ' . $virtualHostDirectory . '/system/');
+		}
+		if (!mkdir($virtualHostDirectory . '/system/logs/', 0777, TRUE)) {
+			throw new \Exception('Failed to create folder: ' . $virtualHostDirectory . '/system/logs/');
+		}
+		if (!mkdir($virtualHostDirectory . '/system/sessions/', 0777, TRUE)) {
+			throw new \Exception('Failed to create folder: ' . $virtualHostDirectory . '/system/sessions/');
+		}
+		if (!mkdir($virtualHostDirectory . '/system/sockets/', 0777, TRUE)) {
+			throw new \Exception('Failed to create folder: ' . $virtualHostDirectory . '/system/sockets/');
+		}
+		if (!mkdir($virtualHostDirectory . '/htdocs/', 0777, TRUE)) {
+			throw new \Exception('Failed to create folder: ' . $virtualHostDirectory . '/htdocs/');
+		}
+
+	}
+
+	private function taskCreateFpmFile() {
+		$this->output->writeln('Creating FPM File');
+		$template = new \Langeland\Bazinga\Service\TemplateService(__DIR__ . '/../../../../Resources/fpm.template');
+		$template->setVar('installationName', $this->virtualHostConfiguration['name']);
+		$template->setVar('installationRoot', $this->configuration['hostroot.directory'] . '/' . $this->virtualHostConfiguration['pseudoGroup'] . '/' . $this->virtualHostConfiguration['name']);
+
+		$template->setVar('user', $this->virtualHostConfiguration['user']);
+		$template->setVar('group', $this->virtualHostConfiguration['group']);
+
+
+		$fileContent = $template->render();
+		$file = tempnam('/tmp', 'bazinga');
+		file_put_contents($file, $fileContent);
+
+		$command = vsprintf(
+			'sudo mv %s %s',
+			array(
+				$file,
+				$this->configuration['fpmPool.directory'] . '/' . $this->virtualHostConfiguration['name'] . '.pool',
+			)
+		);
+
+		exec($command);
+
+	}
+
+	private function taskCreateVirtualHostFile() {
+		$this->output->writeln('Creating VirtualHost File');
+		$template = new \Langeland\Bazinga\Service\TemplateService(__DIR__ . '/../../../../Resources/VirtualHost.template');
+		$template->setVar('installationName', $this->virtualHostConfiguration['name']);
+		$template->setVar('installationRoot', $this->configuration['hostroot.directory'] . '/' . $this->virtualHostConfiguration['pseudoGroup'] . '/' . $this->virtualHostConfiguration['name']);
+		$fileContent = $template->render();
+		$file = tempnam('/tmp', 'bazinga');
+		file_put_contents($file, $fileContent);
+
+		$command = vsprintf(
+			'sudo mv %s %s',
+			array(
+				$file,
+				$this->configuration['sitesAvailable.directory'] . '/' . $this->virtualHostConfiguration['name'] . '.conf',
+			)
+		);
+
+		exec($command);
+
+	}
+
+	private function taskCreateSystemUserAndGroup(){
+		$this->output->writeln('Creating Syetem user and group (Not implemented)');
+
+	}
+
+	private function taskEnableSite(){
+		$this->output->writeln('Enabling apache site (Not implemented)');
+
+	}
+
+	private function taskRestartApache(){
+		$this->output->writeln('Creating Restarting apache (Not implemented)');
+
+	}
+
+
 
 }
 
---
+++ Libraries/Langeland/Bazinga/Service/TemplateService.php
@@ -0,0 +1,36 @@
+<?php
+namespace Langeland\Bazinga\Service;
+
+class TemplateService {
+
+	private $template;
+
+	private $vars;
+
+	public function __construct($templatePathAndName) {
+		$this->setTemplatePathAndName($templatePathAndName);
+	}
+
+	public function setTemplatePathAndName($templatePathAndName) {
+		if (file_exists($templatePathAndName)) {
+			$this->template = file_get_contents($templatePathAndName);
+		} else {
+			$this->template = $templatePathAndName;
+		}
+	}
+
+	public function setVar($var, $content) {
+		$this->vars[$var] = $content;
+	}
+
+	public function render() {
+		$this->replaceAll();
+		return $this->template;
+	}
+
+	private function replaceAll() {
+		foreach ($this->vars as $var => $content) {
+			$this->template = str_replace('{' . $var . '}', $content, $this->template);
+		}
+	}
+}
