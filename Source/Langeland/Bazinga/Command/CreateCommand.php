73e7b51fdabcd90b3b5d22e8c1bacc2ca085ca51
Mads Høgstedt Danquah <mads@danquah.dk>
2016-01-06 20:03:42 +0100

    Create login and web-user

--- Source/Langeland/Bazinga/Command/CreateCommand.php
+++ Source/Langeland/Bazinga/Command/CreateCommand.php
@@ -2,13 +2,19 @@
 
 namespace Langeland\Bazinga\Command;
 
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
 class CreateCommand extends \Langeland\Bazinga\Command\AbstractCommand {
 	protected $virtualHostConfiguration = array();
 
 	protected function configure() {
 		$this
 			->setName('create')
-			->setDescription('Create a new vhost');
+			->setDescription('Create a new vhost')
+			->addOption('name', NULL, InputOption::VALUE_OPTIONAL)
+			->addOption('group', NULL, InputOption::VALUE_OPTIONAL);
 	}
 
 	protected function execute(\Symfony\Component\Console\Input\InputInterface $input, \Symfony\Component\Console\Output\OutputInterface $output) {
@@ -22,49 +28,46 @@ class CreateCommand extends \Langeland\Bazinga\Command\AbstractCommand {
 
 		$helper = $this->getHelper('question');
 
+		if (!empty($input->getOption('name'))) {
+			$this->virtualHostConfiguration['name'] = trim($input->getOption('name'));
+		} else {
+			$question = new \Symfony\Component\Console\Question\Question('Input name of the installation. This will be used to create user and group (lowercase letters and numbers only): ');
+			$question->setValidator(function ($answer) {
+				if ($answer == '') {
+					throw new \RuntimeException('Installation name cannot be empty');
+				}
+				if (in_array($answer . '_web', $this->getSystemUsers())) {
+					throw new \RuntimeException('User exists');
+				}
+
+				if (in_array($answer . '_grp', $this->getSystemGroups())) {
+					throw new \RuntimeException('Group exists');
+				}
+
+				return $answer;
+			});
+
+			$this->virtualHostConfiguration['name'] = trim($helper->ask($input, $output, $question));
+		}
 		/***************************************************************************************************************
 		 *
 		 **************************************************************************************************************/
-		$question = new \Symfony\Component\Console\Question\Question('Input name of the installation. This will be used to create user and group (lowercase letters and numbers only): ');
-		$question->setValidator(function ($answer) {
-			if ($answer == '') {
-				throw new \RuntimeException('Installation name cannot be empty');
-			}
-			if (in_array($answer . '_web', $this->getSystemUsers())) {
-				throw new \RuntimeException('User exists');
-			}
-
-			if (in_array($answer . '_grp', $this->getSystemGroups())) {
-				throw new \RuntimeException('Group exists');
-			}
-
-			return $answer;
-		});
-
-		$this->virtualHostConfiguration['name'] = trim($helper->ask($input, $output, $question));
-		$this->virtualHostConfiguration['user'] = $this->virtualHostConfiguration['name'] . '_web';
+		$this->virtualHostConfiguration['web_user'] = $this->virtualHostConfiguration['name'] . '_web';
+		$this->virtualHostConfiguration['login_user'] = $this->virtualHostConfiguration['name'] . '_login';
 		$this->virtualHostConfiguration['group'] = $this->virtualHostConfiguration['name'] . '_grp';
 
-		/***************************************************************************************************************
-		 *
-		 **************************************************************************************************************/
-		$question = new \Symfony\Component\Console\Question\ChoiceQuestion(
-			'Please select a pseudo group for the domain: ',
-			$this->configuration['pseudo_groups']
-		);
-		$question->setErrorMessage('Pseudo group %s is invalid.');
-		$this->virtualHostConfiguration['pseudoGroup'] = $helper->ask($input, $output, $question);
 
-		/***************************************************************************************************************
-		 *
-		 **************************************************************************************************************/
-		print_r($this->virtualHostConfiguration);
-
-		print_r(array(
-			'rootDirectory' => '/home/hostroot/' . $this->virtualHostConfiguration['pseudoGroup'] . '/' . $this->virtualHostConfiguration['name'],
-			'create user' => $this->virtualHostConfiguration['user'],
-			'create group' => $this->virtualHostConfiguration['group']
-		));
+		// Get the pseudo-group.
+		if (!empty($input->getOption('group'))) {
+			$this->virtualHostConfiguration['pseudoGroup'] = trim($input->getOption('group'));
+		} else {
+			$question = new \Symfony\Component\Console\Question\ChoiceQuestion(
+				'Please select a pseudo group for the domain: ',
+				$this->configuration['pseudo_groups']
+			);
+			$question->setErrorMessage('Pseudo group %s is invalid.');
+			$this->virtualHostConfiguration['pseudoGroup'] = $helper->ask($input, $output, $question);
+		}
 
 		$this->create();
 	}
@@ -96,7 +99,9 @@ class CreateCommand extends \Langeland\Bazinga\Command\AbstractCommand {
 		$this->taskCreateFpmFile();
 		$this->taskCreateVirtualHostFile();
 
+		// Must be run after taskCreateDirectories().
 		$this->taskCreateSystemUserAndGroup();
+
 		$this->taskEnableSite();
 		$this->taskRestartApache();
 	}
@@ -106,6 +111,8 @@ class CreateCommand extends \Langeland\Bazinga\Command\AbstractCommand {
 
 		$pseudoGroupDirectory = $this->configuration['directories']['hostroot'] . '/' . $this->virtualHostConfiguration['pseudoGroup'];
 		$virtualHostDirectory = $pseudoGroupDirectory . '/' . $this->virtualHostConfiguration['name'];
+		$this->virtualHostConfiguration['login_homedir'] = $virtualHostDirectory;
+		$this->virtualHostConfiguration['web_homedir'] = $virtualHostDirectory . '/htdocs/';
 
 		if (!is_dir($this->configuration['directories']['hostroot'] . '/' . $this->virtualHostConfiguration['pseudoGroup'])) {
 			die('Missing pseudoGroup dir');
@@ -129,8 +136,8 @@ class CreateCommand extends \Langeland\Bazinga\Command\AbstractCommand {
 		if (!mkdir($virtualHostDirectory . '/system/sockets/', 0777, TRUE)) {
 			throw new \Exception('Failed to create folder: ' . $virtualHostDirectory . '/system/sockets/');
 		}
-		if (!mkdir($virtualHostDirectory . '/htdocs/', 0777, TRUE)) {
-			throw new \Exception('Failed to create folder: ' . $virtualHostDirectory . '/htdocs/');
+		if (!mkdir($this->virtualHostConfiguration['web_homedir'], 0777, TRUE)) {
+			throw new \Exception('Failed to create folder: ' . $this->virtualHostConfiguration['web_homedir']);
 		}
 
 	}
@@ -154,7 +161,7 @@ class CreateCommand extends \Langeland\Bazinga\Command\AbstractCommand {
 		$template = new \Langeland\Bazinga\Service\TemplateService(__DIR__ . '/../../../../Resources/fpm.template');
 		$template->setVar('installationName', $this->virtualHostConfiguration['name']);
 		$template->setVar('installationRoot', $this->configuration['directories']['hostroot'] . '/' . $this->virtualHostConfiguration['pseudoGroup'] . '/' . $this->virtualHostConfiguration['name']);
-		$template->setVar('user', $this->virtualHostConfiguration['user']);
+		$template->setVar('user', $this->virtualHostConfiguration['web_user']);
 		$template->setVar('group', $this->virtualHostConfiguration['group']);
 		$fileContent = $template->render();
 
@@ -165,9 +172,46 @@ class CreateCommand extends \Langeland\Bazinga\Command\AbstractCommand {
 		symlink($file, $this->configuration['directories']['fpm_pool'] . '/' . $this->virtualHostConfiguration['name'] . '.pool');
 	}
 
+	/**
+	 * Adds a web and login system-user.
+	 *
+	 * Both are in the <name>_grp group
+	 *
+	 * _web has writeaccess to the webroot and nothing else
+	 * _login has write-access to
+	 */
 	private function taskCreateSystemUserAndGroup() {
-		$this->output->writeln('Creating Syetem user and group (Not implemented)');
+		// Create the web-user.
+		$group_esc = escapeshellarg($this->virtualHostConfiguration['group']);
+		$process = new \Symfony\Component\Process\Process('addgroup --force-badname ' . $group_esc);
+		try {
+			$process->mustRun();
+		} catch (ProcessFailedException $e) {
+			$this->output->writeln('Could not create the group ' . $this->virtualHostConfiguration['group'] . ': ' . $e->getMessage());
+			throw $e;
+		}
 
+		$web_homedir_esc = escapeshellarg($this->virtualHostConfiguration['web_homedir']);
+		$web_user_esc = escapeshellarg($this->virtualHostConfiguration['web_user']);
+
+		$process = new \Symfony\Component\Process\Process('adduser --force-badname --home ' . $web_homedir_esc . ' --shell /bin/false --disabled-login --gecos \'\' --ingroup ' . $group_esc . ' ' . $web_user_esc);
+		try {
+			$process->mustRun();
+		} catch (ProcessFailedException $e) {
+			$this->output->writeln('Unable to create user ' . $this->virtualHostConfiguration['web_user'] . ': ' . $e->getMessage());
+			throw $e;
+		}
+
+		// Create the login-user.
+		$login_homedir_esc = escapeshellarg($this->virtualHostConfiguration['login_homedir']);
+		$login_user_esc = escapeshellarg($this->virtualHostConfiguration['login_user']);
+		$process = new \Symfony\Component\Process\Process('adduser --force-badname --home ' . $login_homedir_esc . ' --disabled-login --gecos \'\' --ingroup ' . $group_esc . ' ' . $login_user_esc);
+		try {
+			$process->mustRun();
+		} catch (ProcessFailedException $e) {
+			$this->output->writeln('Unable to create user ' . $this->virtualHostConfiguration['login_user'] . ': ' . $e->getMessage());
+			throw $e;
+		}
 	}
 
 	private function taskEnableSite() {
